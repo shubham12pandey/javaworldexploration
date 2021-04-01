851791e7554f843d076a50304537e9bfffedad61
shubham12pandey <zigshubham89@gmail.com>
2021-04-01 19:03:23 +0530

    pattern program

---
+++ pattern.java
@@ -0,0 +1,18 @@
+/**
+ * pattern
+ */
+public class pattern {
+
+    public static void main(String[] args) {
+        int number=1;
+        for(int i=1;i<=10;i++){
+            for(int j=1;j<=10-i;j++){
+                System.out.print("  ");
+            }
+            for(int j=1;j<=i;j++){
+                System.out.print(number++ +"  ");
+            }
+            System.out.println();
+        }
+    }
+}
